a6406937562ff60d99b9ed0d73981e076cf77849
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2023-12-04 19:04:22 +0200

    bugfix

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -283,7 +283,7 @@ class Cat():
 
         #white patterns
 
-        if type(white_pattern[0]) == list:
+        if white_pattern and type(white_pattern[0]) == list:
             white_pattern = white_pattern[0]
         
         def GenerateWhite(KIT, KITgrade, vit, white_pattern):
