ab9dcd67a3c76f04cccb33b61b9177d01518363d
selkirks <leafylugia@gmail.com>
2024-06-11 22:16:36 -0400

    wawa

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -331,10 +331,6 @@ class Pelt:
             elif self.cat_sprites['senior'] == 5:
                 self.cat_sprites['senior'] = 14
         
-        if self.pattern in convert_dict["old_tortie_patches"]:
-            old_pattern = self.pattern
-            self.pattern = convert_dict["old_tortie_patches"][old_pattern][1]
-
             # If the pattern is old, there is also a chance the base color is stored in
             # tortiecolour. That may be different from the pelt color ("main" for torties)
             # generated before the "ginger-on-ginger" update. If it was generated after that update,
