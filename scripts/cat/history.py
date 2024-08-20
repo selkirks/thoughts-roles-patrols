a1fc75b3a334677c1965b17014f963ef6bcc1287
selkirks <leafylugia@gmail.com>
2024-08-19 22:53:55 -0400

    add history strings

--- scripts/cat/history.py
+++ scripts/cat/history.py
@@ -267,7 +267,26 @@ class History:
             SkillPath.DREAM: ["understanding dreams"],
             SkillPath.CLAIRVOYANT: ["predicting the future"],
             SkillPath.PROPHET: ["understanding prophecies"],
-            SkillPath.GHOST: ["connecting to the afterlife"]
+            SkillPath.GHOST: ["connecting to the afterlife"],
+            SkillPath.GARDENER: ["tending to the Clan's garden"],
+            SkillPath.UNKNOWN: ["connecting to the unknown residence"],
+            SkillPath.WAKEFUL: ["vigilance"],
+            SkillPath.DELIVERER: ["delivering kits"],
+            SkillPath.DECORATOR: ["decorating dens"],
+            SkillPath.LEADERSHIP: ["leading"],
+            SkillPath.AGILE: ["agility"],
+            SkillPath.STEALTHY: ["stealth"],
+            SkillPath.MEMORY: ["memorization"],
+            SkillPath.MESSENGER: ["delivering messages"],
+            SkillPath.ASSIST: ["assisting Clanmates"],
+            SkillPath.HISTORIAN: ["reciting history"],
+            SkillPath.BOOKMAKER: ["creating stories"],
+            SkillPath.TUNNELER: ["tunneling"],
+            SkillPath.PATIENT: ["calmness"],
+            SkillPath.DETECTIVE: ["solving mysteries"],
+            SkillPath.HERBALIST: ["creating remedies"],
+            SkillPath.CHEF: ["preparing food"],
+            SkillPath.PRODIGY: ["learning"]
         }
 
         for _ment in cat.history.mentor_influence["skill"]:
